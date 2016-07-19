8a5f69bd178288056ea708822d1ab3920e4d0708
bhelms <blake@blakehelms.net>
2016-07-19 12:38:21 -0500

    Added solution for mobile applications

---
+++ src/CodeConf.NET.Mobile/CodeConf.NET.Mobile/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CodeConf.NET.Mobile
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
