835c6b0fcf774dbfa368eeba49e07acd8f05e21f
bhelms <blake@blakehelms.net>
2016-07-23 17:24:53 -0500

    Added some dummy data and styling
    Added some dummy data to the consumer facing session list page and
    added some basic styling.

---
+++ src/ConCode.NET.Web/Models/SessionListModel.cs
@@ -0,0 +1,15 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ConCode.NET.Core.Domain;
+
+namespace ConCode.NET.Web.Models.ViewModels
+{
+      public class SessionListViewModel
+    {
+        public List<Session> SessionList;
+    }
+}
