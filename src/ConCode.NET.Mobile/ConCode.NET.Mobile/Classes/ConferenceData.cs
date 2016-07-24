40e22b1babe7b9ba9b934d8f6ac8bea8366d2ebd
bhelms <blake@blakehelms.net>
2016-07-24 13:57:46 -0500

    VERY rough wire up of the session list. Most of this will change but I want to check-in while I have it working.

---
+++ src/ConCode.NET.Mobile/ConCode.NET.Mobile/Classes/ConferenceData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ConCode.NET.Core.Domain;
+
+namespace ConCode.NET.Mobile
+{
+
+
+	public class ConferenceData
+	{
+		public List<Session> Sessions { get; private set; }
+		HttpClient client;
+		string Uri = "http://localhost:5000/api/session";
+
+		public ConferenceData()
+		{
+			client = new HttpClient();
+			client.MaxResponseContentBufferSize = 256000;
+		}
+
+		public Task<List<Session>> GetSessionsAsync()
+		{
+			return RefreshDataAsync();
+		}
+
+		public async Task<List<Session>> RefreshDataAsync()
+		{
+			Sessions = new List<Session>();
+
+			try
+			{
+				var uri = new Uri(string.Format(Uri, string.Empty));
+
+				var response = await client.GetAsync(uri);
+				if (response.IsSuccessStatusCode)
+				{
+					var content = await response.Content.ReadAsStringAsync();
+					Sessions = JsonConvert.DeserializeObject<List<Session>>(content);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(@"ERROR {0}", ex.Message);
+			}
+
+			return Sessions;
+		}
+	}
+
+}
+
