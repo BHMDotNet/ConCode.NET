a9121b84ed10ee1f4fd6bc3364cbd8da2c1243c9
bhelms <blake@blakehelms.net>
2016-07-23 22:53:03 -0500

    Interim check-in of styling for the speaker details page.

--- src/ConCode.NET.Web/Controllers/SpeakerController.cs
+++ src/ConCode.NET.Web/Controllers/SpeakerController.cs
@@ -12,30 +12,30 @@ namespace ConCode.NET.Web.Controllers
             return new Speaker
             {
                 Id = 1,
-                Bio = "A very interesting story needs supplying here.",
+                Bio = "I recommend you don't fire until you're within 40,000 kilometers. About four years. I got tired of hearing how young I looked. Now we know what they mean by 'advanced' tactical training. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf.",
                 BlogUri = new Uri("http://theverybestblog.com"),
                 CreatedAt = DateTime.Now,
-                FacebookProfile = string.Empty,
-                FirstName = "John",
-                LastName = "Doe",
+                FacebookProfile = "blakehelms",
+                FirstName = "Blake",
+                LastName = "Helms",
                 LastUpdated = DateTime.Now,
-                LinkedInProfile = string.Empty,
-                Photo = null,
+                LinkedInProfile = "blakehelms",
+                Photo = new Uri("https://pbs.twimg.com/profile_images/287277250/WebReadyColorProfilePhoto.jpg"),
                 Tagline = "Someone very interesting",
-                TwitterHandle = string.Empty,
+                TwitterHandle = "helmsb",
                 Talks = new List<Talk>()
                 {
                     new Talk
                     {
                         Title = "The Color Tuple",
-                        Abstract = "Everyone needs a helping hand with Tuples.",
+                        Abstract = "Fate protects fools, little children and ships named Enterprise. I guess it's better to be lucky than good. Why don't we just give everybody a promotion and call it a night - 'Commander'?",
                         Level = TalkLevel.Intermediate,
                         TimesPresented = 1
                     },
                     new Talk
                     {
                         Title = "Asyncronous In A Syncronous World",
-                        Abstract = "How to break through the bonds of syncronous operations.",
+                        Abstract = "Well, that's certainly good to know. The Enterprise computer system is controlled by three primary main processor cores, cross-linked with a redundant melacortz ramistat, fourteen kiloquad interface modules.",
                         Level = TalkLevel.Advanced,
                         TimesPresented = 4
                     }
