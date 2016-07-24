14ac74817e8fdeb29fcb9f4d31ededef15338834
Brandon Russell <BRussell@ebsco.com>
2016-07-23 19:43:06 -0500

    * Added Web project reference to ConCode.NET.Tests * Added Index view action Test for SpeakerController * Added Core project reference to ConCode.NET.Web * Added initial Index action to SpeakerController * Added initial Index view page

---
+++ src/ConCode.NET.Tests/Web/Controllers/Speaker/When_requesting_Index_on_SpeakerController.cs
@@ -0,0 +1,40 @@
+﻿using ConCode.NET.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using SpeakerPoco = ConCode.NET.Core.Domain.Speaker;
+
+namespace CodeConf.NET.Tests.Web.Controllers.Speaker
+{
+    public class When_requesting_Index_on_SpeakerController
+    {
+        private SpeakerController _controller;
+        private ViewResult _result;
+
+        public When_requesting_Index_on_SpeakerController()
+        {
+            _controller = new SpeakerController();
+        }
+
+        public void Because()
+        {
+            _result = (ViewResult)_controller.Index();
+        }
+
+        [Fact]
+        public void Should_provide_valid_Speaker_model_when_requesting_Index_view()
+        {
+            // Setup variables
+
+            Because();
+
+            // Checks
+            Assert.NotNull(_result);
+            Assert.NotNull(_result.Model);
+
+            var resultModel = (SpeakerPoco)_result.Model;
+            Assert.NotNull(resultModel);
+            Assert.False(string.IsNullOrWhiteSpace(resultModel.FirstName));
+            Assert.True(resultModel.Id > 0);
+        }
+    }
+}
--- src/ConCode.NET.Web/Controllers/SpeakerController.cs
+++ src/ConCode.NET.Web/Controllers/SpeakerController.cs
@@ -1,16 +1,54 @@
+using ConCode.NET.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ConCode.NET.Web.Controllers
 {
     public class SpeakerController : Controller
     {
+        public static Speaker CreateSpeakerPocoTemplate()
+        {
+            return new Speaker
+            {
+                Id = 1,
+                Bio = "A very interesting story needs supplying here.",
+                BlogUri = new Uri("http://theverybestblog.com"),
+                CreatedAt = DateTime.Now,
+                FacebookProfile = string.Empty,
+                FirstName = "John",
+                LastName = "Doe",
+                LastUpdated = DateTime.Now,
+                LinkedInProfile = string.Empty,
+                Photo = null,
+                Tagline = "Someone very interesting",
+                TwitterHandle = string.Empty,
+                Talks = new List<Talk>()
+                {
+                    new Talk
+                    {
+                        Title = "The Color Tuple",
+                        Abstract = "Everyone needs a helping hand with Tuples.",
+                        Level = TalkLevel.Intermediate,
+                        TimesPresented = 1
+                    },
+                    new Talk
+                    {
+                        Title = "Asyncronous In A Syncronous World",
+                        Abstract = "How to break through the bonds of syncronous operations.",
+                        Level = TalkLevel.Advanced,
+                        TimesPresented = 4
+                    }
+                }
+            };
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //TODO: Need to get the Speaker object from somewhere right?
+            var speaker = CreateSpeakerPocoTemplate();
+
+            return View(speaker);
         }
     }
 }
