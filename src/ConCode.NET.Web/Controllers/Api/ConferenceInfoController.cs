4c5cfd380badabb411fcbca062fd82a30640abaa
Steven Williams <stgwilli@gmail.com>
2016-07-30 12:35:44 -0500

    conference api controller is working with some stub data in the in memory conference provider.

--- src/ConCode.NET.Web/Controllers/Api/ConferenceInfoController.cs
+++ src/ConCode.NET.Web/Controllers/Api/ConferenceInfoController.cs
@@ -10,6 +10,11 @@ namespace ConCode.NET.Web.Controllers.Api
     {
         private IConferenceService conferenceService;
 
+        public ConferenceController(IConferenceService conferenceService)
+        {
+            this.conferenceService = conferenceService;
+        }
+
         [HttpGet]
         public ConferenceInfo Get()
         {
