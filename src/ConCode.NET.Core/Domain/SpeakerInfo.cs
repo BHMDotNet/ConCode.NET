1bbb6f4e42894009468956d7b07ba940f1d637f7
Robb Schiefer <rschiefer@live.com>
2016-07-30 07:39:00 -0500

    added default empty talk list

--- src/ConCode.NET.Core/Domain/SpeakerInfo.cs
+++ src/ConCode.NET.Core/Domain/SpeakerInfo.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 namespace ConCode.NET.Core.Domain
 {
     public class SpeakerInfo
-   {
+    {
         public long Id { get; set; }
-        public IEnumerable<Talk> Talks { get; set; }
+        List<Talk> _talks;
+        public List<Talk> Talks
+        {
+            get
+            {
+                if (_talks == null)
+                {
+                    return new List<Talk>();
+                }
+                return _talks;
+            }
+            set { _talks = value; }
+        }
 
         public string Tagline { get; set; }
         public long UserId { get; internal set; }
