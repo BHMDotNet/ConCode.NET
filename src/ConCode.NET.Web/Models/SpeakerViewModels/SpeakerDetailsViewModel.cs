88c58af456b2d8a0acbbf4cea03d37e6e113f359
samirbehara <samirbehara@gmail.com>
2016-07-31 11:17:06 -0500

    Displaying the Speaker List and Speaker Details on click of the Speakers tab in the HomePage

---
+++ src/ConCode.NET.Web/Models/SpeakerViewModels/SpeakerDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ConCode.NET.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConCode.NET.Web.Models.SpeakerViewModels
+{
+    public class SpeakerDetailsViewModel
+    {
+        public User speakerInfo { get; set; }
+    }
+}
