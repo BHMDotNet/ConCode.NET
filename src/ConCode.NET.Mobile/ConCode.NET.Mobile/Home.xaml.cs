64f03059197d448848f3af4f51aa34ef28a38d5b
bhelms <blake@blakehelms.net>
2016-07-31 17:10:05 -0500

    Basic layout of home screen on mobile

--- src/ConCode.NET.Mobile/ConCode.NET.Mobile/Home.xaml.cs
+++ src/ConCode.NET.Mobile/ConCode.NET.Mobile/Home.xaml.cs
@@ -7,9 +7,26 @@ namespace ConCode.NET.Mobile
 {
 	public partial class Home : ContentPage
 	{
+		private ConferenceInfoModel _conferenceInfo;
+
 		public Home()
 		{
 			InitializeComponent();
+		}
+
+
+		protected async override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (_conferenceInfo == null)
+			{
+				var cd = new ConferenceData();
+				_conferenceInfo = await cd.GetConferenceInfoAsync();
+
+				Title.Text = _conferenceInfo.Name;
+				Description.Text = _conferenceInfo.Description;
+			}
 		}
 	}
 }
